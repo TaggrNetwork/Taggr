cb29b328be577646aa65ac21f4a617b9716bd06c
X <x@taggr.link>
2023-03-05 13:52:39 +0100

    minor fix

--- src/frontend/src/icons.jsx
+++ src/frontend/src/icons.jsx
@@ -12,7 +12,7 @@ export const Infinity = () =>
     </svg>;
 
 export const Comment = ({classNameArg}) =>
-    <svg xmlns="http://www.w3.org/2000/svg" className="vertically_aligned" width={14} height={14} className={classNameArg || null} viewBox="0 0 16 14">
+    <svg xmlns="http://www.w3.org/2000/svg" width={14} height={14} className={`vertically_aligned ${classNameArg || ""}`} viewBox="0 0 16 14">
         <path fillRule="evenodd" d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z"/>
     </svg>;
 
