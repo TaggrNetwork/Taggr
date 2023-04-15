6b886386faa222b3b1f6666b1def8f118ece67aa
X <126339296+taggrx@users.noreply.github.com>
2023-04-15 09:52:39 +0200

    Serde Compatibility Fix (#76)
    Co-authored-by: X <x@taggr.link>

--- src/backend/env/post.rs
+++ src/backend/env/post.rs
@@ -10,7 +10,9 @@ pub struct Poll {
     pub options: Vec<String>,
     pub votes: BTreeMap<u16, BTreeSet<UserId>>,
     pub deadline: u64,
+    #[serde(default)]
     pub weighted_by_karma: BTreeMap<u16, Karma>,
+    #[serde(default)]
     pub weighted_by_tokens: BTreeMap<u16, Token>,
 }
 
