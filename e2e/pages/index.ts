36188ee07f0666f4e1a5e07ad5e4d684f9af36be
Nathan Mc Grath <contact@nathanos.dev>
2023-05-22 16:44:37 +0100

    post creation with image e2e test (#116)
    Co-authored-by: X <126339296+taggrx@users.noreply.github.com>

--- e2e/pages/index.ts
+++ e2e/pages/index.ts
@@ -4,4 +4,5 @@ export * from "./home_page";
 export * from "./internet_identity_page";
 export * from "./mint_initial_cycles_page";
 export * from "./new_post_page";
+export * from "./post_page";
 export * from "./settings_page";
--- e2e/pages/new_post_page.ts
+++ e2e/pages/new_post_page.ts
@@ -1,13 +1,16 @@
 import { Locator, Page } from "@playwright/test";
+import { PostPage } from "./post_page";
 
 export class NewPostPage {
   public readonly cycleCost: Locator;
   private readonly inputTextArea: Locator;
+  private readonly filePickerButton: Locator;
   private readonly sendButton: Locator;
 
   constructor(private readonly page: Page) {
     this.cycleCost = page.getByTestId("cycle-cost");
     this.inputTextArea = page.locator("textarea");
+    this.filePickerButton = page.getByTestId("file-picker");
     this.sendButton = page.locator("button", { hasText: "SEND" });
   }
 
@@ -19,15 +22,26 @@ export class NewPostPage {
     return await this.inputTextArea.textContent();
   }
 
+  public async addImage(imagePath: string): Promise<void> {
+    const [fileChooser] = await Promise.all([
+      this.page.waitForEvent("filechooser"),
+      this.filePickerButton.click(),
+    ]);
+
+    await fileChooser.setFiles([imagePath]);
+  }
+
   public async getPostContent(): Promise<string> {
     return await this.inputTextArea.inputValue();
   }
 
-  public async submit(): Promise<void> {
+  public async submit(): Promise<PostPage> {
     await this.sendButton.click();
 
     // since this navigation is asynchronous and not a result of directly
     // clicking an anchor tag, Playwright does not know that it needs to wait
     await this.page.waitForURL("**/#/post/*");
+
+    return new PostPage(this.page);
   }
 }
