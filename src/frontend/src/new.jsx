cffcae5fa4a01c9b166dda1a362f6590c5ed9065
X <x@taggr.link>
2023-07-24 09:16:03 +0200

    removes redundant cache reloading

--- src/frontend/src/new.jsx
+++ src/frontend/src/new.jsx
@@ -28,7 +28,7 @@ export const PostSubmissionForm = ({ id, repost }) => {
                 text,
                 blobs,
                 patch,
-                optionalRealm,
+                optionalRealm
             );
             if ("Err" in response) {
                 alert(`Error: ${response.Err}`);
@@ -41,14 +41,13 @@ export const PostSubmissionForm = ({ id, repost }) => {
                 blobs,
                 [],
                 optionalRealm,
-                encodeExtension(extension),
+                encodeExtension(extension)
             );
             if ("Err" in result) {
                 return alert(`Error: ${result.Err}`);
             }
             postId = result.Ok;
         }
-        await window.reloadCache();
         window.cleanUICache();
         // If we have created the very first post, we need to fetch the bucket ids from the backend.
         if (backendCache.stats.buckets.length == 0 && blobs.length > 0)
