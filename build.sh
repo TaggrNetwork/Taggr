913baa0fa928ead3643d87e50dbd6b08da798939
X <x@taggr.link>
2023-10-07 14:00:07 +0200

    update ic-wasm

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo "Features: $FEATURES"
 for pkg in $1; do
     cargo build -q --target wasm32-unknown-unknown --release --package $pkg --features "$FEATURES" --locked
     WASM_FILE=target/wasm32-unknown-unknown/release/$pkg.wasm
-    ic-wasm $WASM_FILE -o $WASM_FILE shrink --optimize O3
+    ic-wasm $WASM_FILE -o $WASM_FILE shrink
     gzip -nf9v $WASM_FILE
 done
