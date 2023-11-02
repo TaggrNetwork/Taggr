29ec27ebb3124fb9a2f0239317f80ea98143ed5e
X <x@taggr.link>
2023-11-02 08:31:35 +0100

    improves the scroll up button

--- src/frontend/src/index.tsx
+++ src/frontend/src/index.tsx
@@ -332,7 +332,7 @@ const updateDoc = () => {
     document.getElementById("logo_container")?.remove();
     const scroll_up_button = document.createElement("div");
     scroll_up_button.id = "scroll_up_button";
-    scroll_up_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-arrow-up-circle-fill" viewBox="0 0 16 16"><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></svg>`;
+    scroll_up_button.innerHTML = `&#11014;`;
     document.body.appendChild(scroll_up_button);
     window.scrollUpButton = document.getElementById(
         "scroll_up_button",
