ef31c243f6975b7129340a7a5ad3bb074cec4f6a
X <x@taggr.link>
2023-11-04 12:52:59 +0100

    Revert "revert funds to PsychoGamen"
    This reverts commit b4e4f1eff0cdce0f204faf09087379a905bf2938.

--- src/backend/env/user.rs
+++ src/backend/env/user.rs
@@ -59,7 +59,7 @@ pub struct User {
     pub settings: String,
     karma: Karma,
     rewarded_karma: u64,
-    pub cycles: Cycles,
+    cycles: Cycles,
     pub feeds: Vec<BTreeSet<String>>,
     pub followees: BTreeSet<UserId>,
     pub followers: BTreeSet<UserId>,
