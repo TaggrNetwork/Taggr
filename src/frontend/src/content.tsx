19cf6ea7bec738a5cdfb80039018270e06ff6b78
X <x@taggr.link>
2023-11-25 10:18:14 +0100

    fix: tag parsing in code section

--- src/frontend/src/content.tsx
+++ src/frontend/src/content.tsx
@@ -103,7 +103,7 @@ const splitParagraphsAndPics = (
     return result;
 };
 
-const linkOrImageExp = /(!?\[.*?\]\(.*?\))/g;
+const linkOrImageExp = /(!?\[.*?\]\(.*?\)|`.+`)/gs;
 const linkTagsAndUsers = (mdString: string) =>
     mdString
         .split(linkOrImageExp)
