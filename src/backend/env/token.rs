85ac062634b2310d3c60b4657edd630efa7a4461
X <x@taggr.link>
2023-11-30 21:21:22 +0100

    fix: total supply

--- src/backend/env/token.rs
+++ src/backend/env/token.rs
@@ -135,7 +135,7 @@ fn icrc1_fee() -> u128 {
 
 #[query]
 fn icrc1_total_supply() -> u128 {
-    CONFIG.total_supply as u128
+    read(|state| state.balances.values().copied().sum::<u64>() as u128)
 }
 
 #[query]
