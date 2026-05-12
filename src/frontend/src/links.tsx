ec730c9abf5ce07218f7687bf77bcbe1cc9406ba
Anonymous <x@pinbo.eth>
2026-05-12 08:08:59 +0200

    fix links
    fixup

--- src/frontend/src/links.tsx
+++ src/frontend/src/links.tsx
@@ -8,18 +8,29 @@ export const LinksPage = ({}) => {
             <h2>Price Listings</h2>
             <ul>
                 <li>
-                    <a href="https://www.coingecko.com/en/coins/taggr">
+                    <a
+                        target="_blank"
+                        href="https://www.coingecko.com/en/coins/taggr"
+                    >
                         CoinGecko
                     </a>
                 </li>
                 <li>
-                    <a href="https://icpcoins.com/#/token/TAGGR">ICPCoins</a>
+                    <a
+                        target="_blank"
+                        href="https://icpcoins.com/#/token/TAGGR"
+                    >
+                        ICPCoins
+                    </a>
                 </li>
             </ul>
             <h2>{token_symbol} Trading</h2>
             <ul>
                 <li>
-                    <a href="https://app.icpswap.com/info-swap/token/details/6qfxa-ryaaa-aaaai-qbhsq-cai">
+                    <a
+                        target="_blank"
+                        href="https://app.icpswap.com/info-swap/token/details/6qfxa-ryaaa-aaaai-qbhsq-cai"
+                    >
                         ICPSwap exchange
                     </a>
                 </li>
@@ -27,20 +38,30 @@ export const LinksPage = ({}) => {
             <h2>Community-maintained resources</h2>
             <ul>
                 <li>
-                    <a href={`#/realm/000_WELCOME_TO_TAGGR`}>
+                    <a target="_blank" href={`#/realm/000_WELCOME_TO_TAGGR`}>
                         Welcome to Taggr Realm
                     </a>{" "}
                     (helpful for newbies)
                 </li>
                 <li>
-                    <a href={`#/realm/HELP`}>HELP Realm</a>
+                    <a target="_blank" href={`#/realm/HELP`}>
+                        HELP Realm
+                    </a>
                 </li>
                 <li>
-                    <a href="#/feed/@mntyetti+finn">Explanatory material</a> by{" "}
-                    <a href="#/user/MntYetti">MntYetti</a>
+                    <a target="_blank" href="#/feed/@mntyetti+finn">
+                        Explanatory material
+                    </a>{" "}
+                    by{" "}
+                    <a target="_blank" href="#/user/MntYetti">
+                        MntYetti
+                    </a>
                 </li>
                 <li>
-                    <a href="https://oc.app/community/zbg63-qqaaa-aaaar-atika-cai">
+                    <a
+                        target="_blank"
+                        href="https://oc.app/community/zbg63-qqaaa-aaaar-atika-cai"
+                    >
                         OpenChat Community
                     </a>
                 </li>
@@ -48,16 +69,34 @@ export const LinksPage = ({}) => {
             <h2>Development</h2>
             <ul>
                 <li>
-                    <a href={REPO}>Source code repository on Radicle</a>{" "}
-                    (maintained by <a href="#/user/0">X</a>)
+                    <a target="_blank" href={REPO}>
+                        Source code repository on Radicle
+                    </a>{" "}
+                    (maintained by{" "}
+                    <a target="_blank" href="#/user/0">
+                        X
+                    </a>
+                    )
                 </li>
                 <li>
-                    <a href={`https://${staging}`}>Staging environment</a>{" "}
-                    (maintained by <a href="#/user/0">X</a>)
+                    <a target="_blank" href={`https://${staging}`}>
+                        Staging environment
+                    </a>{" "}
+                    (maintained by{" "}
+                    <a target="_blank" href="#/user/0">
+                        X
+                    </a>
+                    )
                 </li>
                 <li>
-                    <a href={`https://${staging2}`}>Staging environment 2</a>{" "}
-                    (maintained by <a href="#/user/935">aligatorr</a>)
+                    <a target="_blank" href={`https://${staging2}`}>
+                        Staging environment 2
+                    </a>{" "}
+                    (maintained by{" "}
+                    <a target="_blank" href="#/user/935">
+                        aligatorr
+                    </a>
+                    )
                 </li>
             </ul>
         </div>
